a5971477d9881cc454f1a21bd041f831d5132232
unknown <kunalparashar2555@gmail.com>
2023-12-12 13:17:46 +0530

    style header part and main section and give transition effect

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9c9b3ac0e31ced2c706c771ec80f7a00&page=1";
 
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=9c9b3ac0e31ced2c706c771ec80f7a00&query="';
